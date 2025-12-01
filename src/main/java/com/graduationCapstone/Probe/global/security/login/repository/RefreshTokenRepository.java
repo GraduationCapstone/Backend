01598567a5ffe75fc49ac33fbf413b2a335d3a58
MAKC <parkone119@gmail.com>
2025-12-01 23:03:45 +0900

    cst-7 feat: RefreshToken 구현

---
+++ src/main/java/com/graduationCapstone/Probe/global/security/login/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.graduationCapstone.Probe.global.security.login.repository;
+
+import com.graduationCapstone.Probe.global.security.login.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+
+}
