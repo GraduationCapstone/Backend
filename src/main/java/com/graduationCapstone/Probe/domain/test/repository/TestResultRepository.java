35d9a15e242452af5dc50f3cdf32a32d786354ca
MAKC <parkone119@gmail.com>
2026-04-18 21:05:12 +0900

    [CST-103] feat: 테스트 관련 레포지토리 기존 코드 수정 및 신규 코드 작성

--- src/main/java/com/graduationCapstone/Probe/domain/test/repository/TestResultRepository.java
+++ src/main/java/com/graduationCapstone/Probe/domain/test/repository/TestResultRepository.java
@@ -3,8 +3,6 @@ package com.graduationCapstone.Probe.domain.test.repository;
 import com.graduationCapstone.Probe.domain.test.entity.TestResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TestResultRepository extends JpaRepository<TestResult, Long>, TestResultRepositoryCustom {
     long countByTestExecution_TestGroup_GroupId(Long groupId);
 }
